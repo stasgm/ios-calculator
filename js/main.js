45113f99e93806d9ddb05627c1f2edce006411c8
stasgm <stasgm@gmail.com>
2018-05-28 20:13:09 +0300

    wip

--- js/main.js
+++ js/main.js
@@ -1,17 +1,33 @@
 const buttons = document.getElementsByClassName("button");
+const output = document.querySelectorAll('.output')[0];
+
+const operators = ['+', '-', 'x', '÷'];
+let curOperation;
+let dotUsed = false;
+
+output.addEventListener('edit', function() {
+    console.log('edit');
+})
 
 for(let i=0; i < buttons.length; i++) {
     let button = buttons[i];
     button.onclick = function(e) {
         let value = e.target.innerHTML
         //console.log(value);
-        const output = document.querySelectorAll('.output')[0];        
         if (value.match(/^\d+$/)) {
             output.innerHTML += value;
         } else {
             switch (value.toUpperCase()) {
-                case 'C': output.innerHTML = '0';
-                break;
+                case 'C': 
+                    output.innerHTML = '0';
+                    dotUsed = false;
+                    break;
+                case ',': 
+                    if (output.innerHTML > '' && !dotUsed) {
+                        output.innerHTML += value;
+                        dotUsed = true;
+                    }
+                    break;
             }
         }
     }
