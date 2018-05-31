7ae2d25fd56ee5cb73e9bc057df50c14f05c7c5b
stasgm <stasgm@gmail.com>
2018-06-01 00:32:14 +0300

    wip

--- js/main.js
+++ js/main.js
@@ -1,12 +1,38 @@
 const buttons = document.getElementsByClassName("button");
 const output = document.querySelectorAll('.output')[0];
 
-const operators = ['+', '-', 'x', '÷'];
-let curOperation;
+const operators = ['+', '-', 'x', '÷', 'c', '='];
+let lastOperation;
 let dotUsed = false;
+let lastSymbol;
 
-function formatOutput() {
-    let num = output.textContent
+document.addEventListener("keydown", function(e) {
+    var key = e.keyCode;
+    //var inputValue = input.value;
+    var shift = e.shiftKey;
+
+    console.log(key);
+});
+
+function updateState() {
+    if (curSymbol > '' ) {
+        // digit or comma
+        output.textContent +=  curSymbol;
+        curSymbol = '';
+    } else if (curOperation > '') {
+        // math operator or 'Clear'
+        switch (curOperation) {
+            case 'c':
+                dotUsed = false;
+                break;        
+            default:
+                break;
+        }
+    }
+
+    curOperation = '';
+    
+    let num = output.textContent;
 
     num = num.replace(/\s/g, ''); 
     if  (num.indexOf(',') === num.length - 1) {
@@ -27,25 +53,28 @@ function formatOutput() {
 }
 
 for(let i=0; i < buttons.length; i++) {
+    curSymbol = '';
     let button = buttons[i];
     button.onclick = function(e) {
         let value = e.target.textContent
         if (value.match(/^\d+$/)) {
-            output.textContent += value;
-        } else {
+            curSymbol = value
+            //output.textContent += value;
+        } else if (operators.indexOf(value)) {
             switch (value.toUpperCase()) {
                 case 'C': 
-                    output.textContent = '0';
-                    dotUsed = false;
+                    //output.textContent = '0';
+                    curOperation = 'c'
                     break;
                 case ',': 
                     if (output.textContent > '' && !dotUsed) {
-                        output.textContent += value;
+                        curSymbol = value
+                        //output.textContent += value;
                         dotUsed = true;
                     }
                     break;
             }
         }
-        formatOutput();        
+        updateState();        
     }
 }
